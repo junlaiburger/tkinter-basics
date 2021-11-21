15407e6a1495773783229aaad299a2076a8ec2c7
junlaiburger <56894688+junlaiburger@users.noreply.github.com>
2021-11-21 11:51:29 -0800

    Update Button widget.py

--- Projects/Button widget.py
+++ Projects/Button widget.py
@@ -1,1 +1,16 @@
+from tkinter import *
 
+# Initialize tk interpreter
+root = Tk()
+
+
+def myClick():
+	myLabel = Label(root, text="Look! I clicked a Button!!")
+	myLabel.pack()
+
+  
+myButton = Button(root, text="Click Me!", command=myClick)
+myButton.pack()
+
+# Starts the event loop of the window
+root.mainloop()
